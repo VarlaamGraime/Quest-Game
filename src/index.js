ff104b39faa2238d7ffcad962d133d228f6b26ac
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2022-12-08 22:07:58 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
